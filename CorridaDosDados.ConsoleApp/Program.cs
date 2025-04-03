9c7b199e9289b24bac528dee88e7c3462b4648bb
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-03 13:14:59 -0300

    Add project files.

---
+++ CorridaDosDados.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CorridaDosDados.ConsoleApp
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
