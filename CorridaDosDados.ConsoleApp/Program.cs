3c6209f05e9b103e102e16bf4b1e32f71c627efd
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-03 13:17:41 -0300

    Criado as variáveis globais

--- CorridaDosDados.ConsoleApp/Program.cs
+++ CorridaDosDados.ConsoleApp/Program.cs
@@ -2,7 +2,22 @@
 {
     internal class Program
     {
-        
+        // cria as variáveis globais
+        int posicaoJogador = 0;
+        int posicaoCpu = 0;
+        string vitorioso = "";
+        int resultadoDado = 0;
+        bool turnoAtivo = true;
+        // cria dicionário que guarda os eventos especiais da partida
+        Dictionary<string, string[]> eventosEspeciais = new Dictionary<string, string[]>
+        {
+            { "broken_bridge", ["-2", "Você chegou em uma ponte quebrada e terá que dar a volta, -2 casas."] },
+            { "paving_path", ["2", "Você chegou em uma rua pavimentada que facilita muito a sua corrida, +2 casas"] },
+            { "ride", ["3", "Você encontrou um carro para te dar carona, +3 casas"] },
+            { "congestion", ["-3", "Você encontrou um congestionamento no caminho que te atrasou muito, -3 casas"] },
+            { "lost", ["0", "Você se perde no caminho, portanto volta onde parou, volta a posição anterior"] },
+            { "+turn", ["+", "Você ultrapassou muito o outro competidor, mais 1 turno"] }
+        };
 
         static void Main(string[] args)
         {
